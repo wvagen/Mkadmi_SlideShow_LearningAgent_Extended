4386a59fb841b39a0c6fad315166efa54de30584
Mouadh Mkadmi <wvagen@gmail.com>
2024-05-03 21:15:14 +0100

    vERSION 4.7

--- Assets/MS_SlideShow_FM/MS_Scripts/MS_SlideShow_Vid.cs
+++ Assets/MS_SlideShow_FM/MS_Scripts/MS_SlideShow_Vid.cs
@@ -25,5 +25,9 @@ public class MS_SlideShow_Vid : MonoBehaviour
     void Update()
     {
         videoMomentTxt.text = ((int)videoPlayer.time).ToString() + " / " + ((int)videoPlayer.length).ToString();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) videoPlayer.playbackSpeed = 1;
+        if (Input.GetKeyDown(KeyCode.Alpha2)) videoPlayer.playbackSpeed = 2;
+        if (Input.GetKeyDown(KeyCode.Alpha3)) videoPlayer.playbackSpeed = 3;
     }
 }
